0bffdb0994cb53ebb0270a27620bbf577d7d8224
deepakunique <deepakunique@gmail.com>
2024-05-23 22:42:01 +0530

    Create 02_04.sql

---
+++ 02_04.sql
@@ -0,0 +1,10 @@
+select * from products order by price;
+
+select * from customer order by customer_name;
+
+
+select * from customer order by customer_name desc;
+
+select * from products order by price, name;
+
+select * from products where is not price>100  order by price;
