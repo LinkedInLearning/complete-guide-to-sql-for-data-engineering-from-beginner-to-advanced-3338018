5c8d3393ce288235219090509120237f90c92e0e
deepakunique <deepakunique@gmail.com>
2024-05-23 22:50:20 +0530

    Create 05_06.sql

---
+++ 05_06.sql
@@ -0,0 +1,17 @@
+select upper(customer_name) from customer;
+
+select lower(customer_name) from customer;
+
+select initcap(customer_name) from customer;
+
+select substring('Hello from Deepak' from 1 for 5);
+
+select substring('Hello from Deepak' from 7 for 5);
+
+select left('Hello from Deepak', 5);
+
+select right('Hello from Deepak', 6);
+
+select position('Alice' in 'Hello from Deepak');
+
+select length('Hello from Deepak');
