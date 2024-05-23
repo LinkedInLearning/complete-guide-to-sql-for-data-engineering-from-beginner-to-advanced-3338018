0b38eb143fec92883edb02639a39a2fdc5796205
deepakunique <deepakunique@gmail.com>
2024-05-23 22:43:05 +0530

    Create 03_02.sql

---
+++ 03_02.sql
@@ -0,0 +1,7 @@
+select * from categories;
+
+select * from products
+INSERT INTO Products (name, price, description, tags, category_id,Supplier)
+VALUES
+    ('Ipad', 100, 'High-performance ipad for professionals', 'electronics, portable, tech', 1, 'SupplierA')
+	
